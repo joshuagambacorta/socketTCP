f095cf8ca685562f1fb1a0c5b319f15487828964
joshua <joshua.gambacorta@gmail.com>
2022-04-05 09:07:54 +0200

    feat: client connection to server.

--- src/joshua/good/student/ClientConnectionTCP.java
+++ src/joshua/good/student/ClientConnectionTCP.java
@@ -1,4 +1,30 @@
 package joshua.good.student;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ClientConnectionTCP {
+
+    public static void main(String[] args) throws IOException {
+        //Porta del server maggiore di 1024
+        int port = 2000;
+
+        //oggetto da usare per fare la connessione TCP
+        Socket socket = null;
+
+        try {
+
+            socket = new Socket("127.0.0.1", port);
+
+        } catch (IOException e) {
+            System.err.println("Errore di I/O nell'istanza del server: "+e);
+        } try {
+                socket.close();
+            } catch (ConnectException e) {
+                System.err.println("Errore in chiusura: "+e);
+            }
+    }
+
 }
